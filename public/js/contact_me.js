126fd7e9f18f4c6ee4e5f44fb915d14be2d4bd2f
Aleksey Kurepin <lesha@kurepin.com>
2020-02-13 23:54:41 +0300

    introducing /api/ namespace

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././messages",
+        url: "././api/messages",
         type: "POST",
         data: {
           message: {
